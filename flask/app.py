c6bfe169c36c547369ead2482d1a01b97f050367
jihong <rlawyd9@kookmin.ac.kr>
2022-06-12 04:18:28 +0900

    flask, api

---
+++ flask/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route('/test', methods=['GET'])
+def test_get():
+   title_receive = request.args.get('title_give')
+   print(title_receive)
+   return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
+
+
+@app.route('/test', methods=['POST'])
+def test_post():
+   title_receive = request.form['title_give']
+   print(title_receive)
+   return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
+
+
+if __name__ == '__main__':
+   app.run('0.0.0.0',port=5000,debug=True)
+
+# GET api 코드
+'''
+@app.route('/test', methods=['GET'])
+def test_get():
+   title_receive = request.args.get('title_give')
+   print(title_receive)
+   return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
+'''
+
+# GET 요청 확인 ajax 코드
+'''
+$.ajax({
+    type: "GET",
+    url: "/test?title_give=봄날은간다",
+    data: {},
+    success: function(response){
+       console.log(response)
+    }
+  })
+'''
